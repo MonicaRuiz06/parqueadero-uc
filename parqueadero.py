e6392143a206fd3db661e21606e674f5367e2641
Cristian0124 <valenciahcristian01@gmail.com>
2026-09-18 19:30:39 -0500

    parqueadero

---
+++ parqueadero.py
@@ -0,0 +1,11 @@
+n = int(input("Cantidad de vehiculos: "))
+
+cupos = 0
+
+while cupos < n and cupos < 30:
+    placa = input("Placa: ")
+    tipo = input("Tipo (E/D/V): ")
+    hora = float(input("Hora: "))
+    permanencia = float(input("Horas de permanencia: "))
+
+    cupos += 1
