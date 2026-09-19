4fd21d0ef0b66543e473d4b2d249f07f3f51088a
Cristian0124 <valenciahcristian01@gmail.com>
2026-09-18 19:43:35 -0500

    prueba de commit :,v

--- parqueadero.py
+++ parqueadero.py
@@ -5,7 +5,9 @@ cupos = 0
 while cupos < n and cupos < 30:
     placa = input("Placa: ")
     tipo = input("Tipo (E/D/V): ")
+
     hora = float(input("Hora(formato 24h): "))
+    
     permanencia = float(input("Horas de permanencia: "))
 
     cupos += 1
