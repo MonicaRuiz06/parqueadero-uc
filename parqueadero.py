2e01bf2dabc4ab40c5298d2ad73f2bf346bb3fd6
MonicaRuiz06 <monica.ruiz.mar16@gmail.com>
2026-09-18 20:03:47 -0500

    feat: implementa calculo de tarifa segun tipo de usuario

--- parqueadero.py
+++ parqueadero.py
@@ -4,10 +4,24 @@ cupos = 0
 
 while cupos < n and cupos < 30:
     placa = input("Placa: ")
-    tipo = input("Tipo (E/D/V): ")
-
-    hora = float(input("Hora(formato 24h): "))
-    
+    tipo = input("Tipo de usuario(E/D/V): ")
+    hora = int(input("Hora(formato 24h): "))
     permanencia = float(input("Horas de permanencia: "))
+
+    # Calculo de tarifa segun tipo de usuario
+    if tipo == "E":
+        if permanencia <= 2:
+            cobro = 0.0
+        else:
+            cobro = (permanencia - 2) * 800
+    elif tipo == "D":
+        cobro = permanencia * 500
+    else:
+        if permanencia <= 1:
+            cobro = 1500.0
+        else:
+            cobro = 1500 + (permanencia - 1) * 1200
+
+    print("Cobro: $", cobro)
 
     cupos += 1
